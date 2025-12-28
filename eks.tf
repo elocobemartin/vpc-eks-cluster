7b0adb8d389eb73f35e1bd82577493a65db09984
Martin Elocobe <elocobemart@gmail.com>
2025-12-28 01:32:18 +0100

    changes on vps and eks

---
+++ eks.tf
@@ -0,0 +1,29 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "my-cluster"
+  kubernetes_version = "1.33"
+
+  # Optional
+  endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = module.vpc.my-vpc_id
+  subnet_ids               = module.vpc.public_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    default = {
+      instance_types = t2.medium
+      min_size     = 2
+      max_size     = 6
+      desired_size = 2
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
