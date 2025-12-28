f7bc08959b5bc64f89b7a7bccefbf98556e79f24
Martin Elocobe <elocobemart@gmail.com>
2025-12-28 02:09:18 +0100

    third commit

--- vpc.tf
+++ vpc.tf
@@ -15,7 +15,7 @@ provider "aws" {
 # Create a VPC using the VPC module
 module "vpc" {
  source = "terraform-aws-modules/vpc/aws"
- name = "my-vpc"
+ name = "vpc"
  cidr = "10.0.0.0/16"
  azs = ["us-east-1a", "us-east-1b"]
  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
